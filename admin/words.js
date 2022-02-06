01e488221352b863a2c00c918dba6f9f51e2098e
xHeitkamp <robin@behlke.tech>
2022-02-06 15:08:48 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'steam-friends adapter settings': {
+		'en': 'Adapter settings for steam-friends',
+		'de': 'Adaptereinstellungen für steam-friends',
+		'ru': 'Настройки адаптера для steam-friends',
+		'pt': 'Configurações do adaptador para steam-friends',
+		'nl': 'Adapterinstellingen voor steam-friends',
+		'fr': "Paramètres d'adaptateur pour steam-friends",
+		'it': "Impostazioni dell'adattatore per steam-friends",
+		'es': 'Ajustes del adaptador para steam-friends',
+		'pl': 'Ustawienia adaptera dla steam-friends',
+		'zh-cn': 'steam-friends的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
